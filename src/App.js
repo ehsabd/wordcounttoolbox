8766c30bf8c0a07c3a0ac94340519d17d532f31c
ehsabd <ehsabd@outlook.com>
2021-01-04 19:49:18 +0330

    uninstall jodit-react

--- src/App.js
+++ src/App.js
@@ -39,7 +39,6 @@ class App extends Component {
         <div className="container pt-3">
           <div className="row">
             <div className="col-sm-8">
-              <Editor></Editor>
             </div>
             <div className="col-sm-4">
               <WordListsContainer itemChanged={this.wordListItemChanged} wordLists={this.state.wordLists}></WordListsContainer>
@@ -76,31 +75,7 @@ class App extends Component {
 
 
 
-
-
-
 }
 
-
-const Editor  = ({}) => {
- 
-  const editor = useRef(null)
-  const [content, setContent] = useState('')
-
-  const config = {
-    readonly: false // all options from https://xdsoft.net/jodit/doc/
-  }
-
-  return (
-    <JoditEditor
-      ref={editor}
-      value={content}
-      config={config}
-      tabIndex={1} // tabIndex of textarea
-      onBlur={newContent => setContent(newContent)} // preferred to use only this option to update the content for performance reasons
-      onChange={newContent => { }}
-    />
-  );
-}
 
 export default App;
