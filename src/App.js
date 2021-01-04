e008a5229b0f2591dc04bf6842aea73cae6186ae
ehsabd <ehsabd@outlook.com>
2021-01-04 19:03:04 +0330

    give WordListsContainer its own file

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import React, { Component } from 'react';
+import {WordListsContainer} from './WordLists.js';
 class App extends Component {
 
   constructor(props){
@@ -33,10 +34,10 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" /> WordCountToolbox
       </header>
-        <div className="container">
+        <div className="container pt-3">
           <div className="row">
             <div className="col-sm-8">
-
+            
             </div>
             <div className="col-sm-4">
               <WordListsContainer itemChanged={this.wordListItemChanged} wordLists={this.state.wordLists}></WordListsContainer>
@@ -73,66 +74,5 @@ class App extends Component {
 
 }
 
-class WordListsContainer extends Component {
-
-  
-
-  render() {
-    return (
-
-      <div className="card">
-        <div className="card-header">
-          Word Lists
-      </div>
-        {
-          this.props.wordLists.map((item, index) => {
-            
-            return <WordListItem key={index} wordListIndex={index} wordListLabel={item.label} wordListWords={item.words} itemChanged={this.props.itemChanged}></WordListItem>
-
-          })
-        }
-
-      </div>
-    )
-  }
-}
-
-class WordListItem extends Component {
-  constructor(props) {
-    super(props)
-
-    this.itemLabelChanged = this.itemLabelChanged.bind(this);
-    this.itemWordsChanged = this.itemWordsChanged.bind(this);
-  }
-
-  itemLabelChanged(e){
-    this.props.itemChanged(this.props.wordListIndex, e.target.value, this.props.wordListWords);
-  }
-
-  itemWordsChanged(e){
-    this.props.itemChanged(this.props.wordListIndex, this.props.wordListLabel, e.target.value);
-  }
-  
-  render() {
-    return (
-      <div>
-        <div className="row">
-          <div className="col-sm-3">
-            <div className="form-group">
-              <input className="form-control" onChange={this.itemLabelChanged} value={this.props.wordListLabel}/>
-            </div>
-          </div>
-          <div className="col-sm-6">
-            <div className="form-group">
-              <textarea className="form-control" onChange={this.itemWordsChanged}>{this.props.wordListWords}</textarea>
-            </div>
-          </div>
-        </div>
-      </div>
-    )
-  }
-
-  
-}
 
 export default App;
---
+++ src/WordLists.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+class WordListsContainer extends Component {
+
+  
+
+    render() {
+      return (
+  
+        <div className="card">
+          <div className="card-header">
+            Word Lists
+        </div>
+          {
+            this.props.wordLists.map((item, index) => {
+              
+              return <WordListItem key={index} wordListIndex={index} wordListLabel={item.label} wordListWords={item.words} itemChanged={this.props.itemChanged}></WordListItem>
+  
+            })
+          }
+  
+        </div>
+      )
+    }
+  }
+  
+  class WordListItem extends Component {
+    constructor(props) {
+      super(props)
+  
+      this.itemLabelChanged = this.itemLabelChanged.bind(this);
+      this.itemWordsChanged = this.itemWordsChanged.bind(this);
+    }
+  
+    itemLabelChanged(e){
+      this.props.itemChanged(this.props.wordListIndex, e.target.value, this.props.wordListWords);
+    }
+  
+    itemWordsChanged(e){
+      this.props.itemChanged(this.props.wordListIndex, this.props.wordListLabel, e.target.value);
+    }
+    
+    render() {
+      return (
+        <div>
+          <div className="row">
+            <div className="col-sm-3">
+              <div className="form-group">
+                <input className="form-control" onChange={this.itemLabelChanged} value={this.props.wordListLabel}/>
+              </div>
+            </div>
+            <div className="col-sm-6">
+              <div className="form-group">
+                <textarea className="form-control" onChange={this.itemWordsChanged}>{this.props.wordListWords}</textarea>
+              </div>
+            </div>
+          </div>
+        </div>
+      )
+    }
+  
+    
+  }
+
+  export {WordListsContainer};
