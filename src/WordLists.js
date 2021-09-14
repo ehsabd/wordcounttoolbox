ca95eb9ef8a17927101b0e2459dc8116c2fc2fd9
ehsabd <ehsabd@outlook.com>
2021-09-14 21:34:28 +0430

    give WordListItem its own file

--- src/WordLists.js
+++ src/WordLists.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { TwitterPicker } from 'react-color';
 import randomColor from 'randomcolor'
+import WordListItem from './WordListItem';
 
 class WordListsContainer extends Component {
 
@@ -46,64 +46,6 @@ class WordListsContainer extends Component {
         </div>
       )
     }
-  }
-  
-  class WordListItem extends Component {
-    constructor(props) {
-      super(props)
-      this.state={};
-      this.itemLabelChanged = this.itemLabelChanged.bind(this);
-      this.itemWordsChanged = this.itemWordsChanged.bind(this);
-      this.itemColorChanged = this.itemColorChanged.bind(this);
-      this.toggleColorPicker = this.toggleColorPicker.bind(this);
-    }
-  
-    itemLabelChanged(e){
-      const label = e.target.value;
-      this.props.itemChanged(this.props.wordListIndex, {label});
-    }
-  
-    itemWordsChanged(e){
-      const words = e.target.value;
-      this.props.itemChanged(this.props.wordListIndex, {words} );
-    }
-
-    itemColorChanged(colorObj){
-      const color = colorObj.hex;
-      this.props.itemChanged(this.props.wordListIndex, {color});
-      this.setState({showColorPicker:false});
-    }
-
-    toggleColorPicker(){
-      const showColorPicker = !this.state.showColorPicker;
-      this.setState({showColorPicker});
-    }
-    
-    render() {
-      return (
-        <li className="list-group-item px-2">
-          <div className="row">
-            <div className="col-11">
-              <div className="form-group">
-                <input className="form-control" onChange={this.itemLabelChanged} value={this.props.wordListLabel} placeholder="Word List Label"/>
-              </div>
-            </div>
-            <span className="color-picker" style={{backgroundColor:this.props.wordListColor}} onClick={this.toggleColorPicker}></span>  
-
-            { this.state.showColorPicker && <div className="col-12 d-flex flex-row-reverse">
-              <TwitterPicker color={this.props.wordListColor} onChangeComplete={this.itemColorChanged} colors={this.props.colorPickerColors} triangle="top-right"></TwitterPicker>
-            </div>}
-            <div className="col-12">
-              <div className="form-group mb-0">
-                <textarea className="form-control" onChange={this.itemWordsChanged} value={this.props.wordListWords} placeholder="Comma-Separated Words"></textarea>
-              </div>
-            </div>
-          </div>
-        </li>
-      )
-    }
-  
-    
   }
 
   export {WordListsContainer};
