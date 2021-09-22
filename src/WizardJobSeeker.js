fcc8b60c193db42f01339845c453869b53768029
ehsabd <ehsabd@outlook.com>
2021-09-22 18:42:38 +0330

    Setup nested route for wizard/jobseeker

---
+++ src/WizardJobSeeker.js
@@ -0,0 +1,10 @@
+import {Component} from 'react';
+
+class WizardJobSeeker extends Component{
+
+    render(){
+        return <div>Hi there job seeker</div>
+    }
+}
+
+export default WizardJobSeeker;
--- src/WizardModal.js
+++ src/WizardModal.js
@@ -1,12 +1,14 @@
 import { Component } from 'react';
-import {Link, useRouteMatch} from 'react-router-dom';
+import {Link, Route, Switch, BrowserRouter as Router} from 'react-router-dom';
 import {withRouter} from 'react-router';
+import WizardJobSeeker from './WizardJobSeeker';
 class WizardModal extends Component {
 
     state={whoOptions:[
-    {text:'Writer',},{text:'Student'},{text:'Job Seeker'},{text:'Teacher'}]}
+    {text:'Writer',},{text:'Student'},{text:'Job Seeker', slug:'jobseeker'},{text:'Teacher'}]}
 
     render() {
+        console.log(this.props);
         const {match:{url, path}} = this.props;
         return  <div className={`modal fade ${(this.props.showModal? 'show d-block':'')}`} tabindex="-1" role="dialog"     aria-hidden="true">
                     <div className="modal-dialog" role="document">
@@ -14,26 +16,33 @@ class WizardModal extends Component {
                             <div className="modal-header border-0">
                             <Link to="/wordcounttoolbox"className="close"><span aria-hidden="true">&times;</span></Link>
                             </div>
-                            <div className="modal-body">
-                                <p className="text-muted text-center">Let us guide you on how WordCountToolbox helps you!</p>
-                                <h2 className="text-center">Who are you?</h2>
-                                <div className="row mt-4">
-                                {
-                                    this.state.whoOptions.map((o)=>{
-                                        return <div className="col-sm-3 col-xs-6"><button class="btn shadow w-100 h-100" style={{backgroundColor:o.isSelected?'gold':'white'}} for="option2">{o.text}
-                                        </button></div>;
-                                    })
-                                }
-                                
-                                </div>
-                                
                             
+                            <Router>
+                                <Switch>
+                                    <Route path={`${path}/jobseeker`} component={WizardJobSeeker}>
+                                    </Route>
+                                    <Route path={`${path}`}>
+                                        <div className="modal-body">
+                                            <p className="text-muted text-center">Let us guide you on how WordCountToolbox helps you!</p>
+                                            <h2 className="text-center">Who are you?</h2>
+                                            <div className="row mt-4">
+                                                {
+                                                    this.state.whoOptions.map((o) => {
+                                                        return <div className="col-sm-3 col-xs-6"><Link to={`${url}/${o.slug}`} class="btn shadow w-100 h-100" style={{ backgroundColor: o.isSelected ? 'gold' : 'white' }} for="option2">{o.text}
+                                                        </Link></div>;
+                                                    })
+                                                }
 
-                            </div>
-                            <div className="modal-footer border-0">
-                            <Link to="/wordcounttoolbox"className="btn btn-secondary">Close</Link>
-                                <button type="button" className="btn btn-primary">Next</button>
-                            </div>
+                                            </div>
+                                        </div>
+                                        <div className="modal-footer border-0">
+                                            <Link to="/wordcounttoolbox"className="btn btn-secondary">Close</Link>
+                                            <button type="button" className="btn btn-primary">Next</button>
+                                        </div>
+                                    </Route>
+                                </Switch>
+                            </Router>
+                            
                         </div>
                     </div>
                 </div>
