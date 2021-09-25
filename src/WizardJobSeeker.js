dcf3067a23a8beba773def7855111842527a96f8
ehsabd <ehsabd@outlook.com>
2021-09-25 22:03:07 +0330

    add job post and resume text inputs to jobseeker wizard

--- src/WizardJobSeeker.js
+++ src/WizardJobSeeker.js
@@ -3,7 +3,22 @@ import {Component} from 'react';
 class WizardJobSeeker extends Component{
 
     render(){
-        return <div>Hi there job seeker</div>
+        return <div className="modal-body">
+            <div className="row">
+                <div className="col-12">
+                    <div className="form-group">
+                        <textarea className="form-control" placeholder="Copy & Paste The Job Description Here ..."></textarea>
+                    </div>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-12">
+                    <div className="form-group">
+                        <textarea className="form-control" placeholder="Copy & Paste Your CV Here ..."></textarea>
+                    </div>
+                </div>
+            </div>
+        </div>
     }
 }
 
